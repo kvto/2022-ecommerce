75d15d0744a00e105551f9ed7673dcb911714475
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-08-17 17:49:09 -0500

    kev - ecommerce

--- src/Components/CheckoutForm/Checkout.js
+++ src/Components/CheckoutForm/Checkout.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import { Paper, Step, StepLabel, Stepper, Typography } from '@mui/material';
+import React, { useState } from 'react'
 import useStyles from "./styles"
-
+import AddressForm from './AddressForm'
+import PaymentForm from './PaymentForm'
 
 export default function Checkout() {
   const classes = useStyles();
+  const [activeStep, setActivestep] = useState(0);
+  const steps = ["Shipping address", "Paymen details"]
+
+  const nextStep = () => setActivestep((prevActivestep) => prevActivestep + 1)
+  const backStep = () => setActivestep((prevActivestep) => prevActivestep - 1)
+
+
+  const Form = () => activeStep === 0 ? <AddressForm /> : <PaymentForm/>
+
   return (
     <>
     <main className={classes.layout}>
-<h1>checkout</h1>
+    <Paper className={classes.paper}>
+<Typography component='h1' variant='h4' align='center'>
+    Checkout
+</Typography>
+    <Stepper activeStep={activeStep} className={classes.stepper}>
+    {steps.map(step=>(
+      <Step key={step}>
+        <StepLabel>{step}</StepLabel>
+      </Step>
+    ))}
+    </Stepper>
+    <Form />
+    </Paper>
+   
     </main>
     </>
   )
