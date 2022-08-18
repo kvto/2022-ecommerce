dc05556a4e698f89f0a862c7cbb4aea9a52b961a
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-08-18 07:22:22 -0500

    kev - ecommerce

--- src/Components/CheckoutForm/AddressForm.js
+++ src/Components/CheckoutForm/AddressForm.js
@@ -1,4 +1,5 @@
 import { Grid, Typography } from '@mui/material';
+import Button from '@mui/material/Button'
 import {useForm, FormProvider} from 'react-hook-form'
 import React from 'react'
 import AddressInput from './AddressInput';
@@ -13,7 +14,13 @@ const AddressForm = () => {
       <form>
         <Grid container spacing={3}>
           <AddressInput required name="firstName" label="First Name"/>
+          <AddressInput required name="lastName" label="Last Name"/>
+          <AddressInput required name="address1" label="Adress"/>
+          <AddressInput required name="city" label="First Name"/>
+          <AddressInput required name="postCode" label="Post Code"/>
         </Grid>
+        <Button type="submit" variant="contained" color="primary">Back ⬅</Button>
+        <Button type="submit" variant="contained" color="primary">Next ➡</Button>
       </form>
       </FormProvider>
      
